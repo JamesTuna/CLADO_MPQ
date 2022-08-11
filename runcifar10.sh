18412c54dd1d3300c41f75636c3a29b7d7061c72
zdeng <zdeng@d-matrix.ai>
2022-08-11 01:40:03 -0700

    more ckpts, general implementation beta version ipynb

--- runcifar10.sh
+++ runcifar10.sh
@@ -1,6 +1,6 @@
-ds='cifar10'
+ds='cifar100'
 mode=0
-for model in "resnet56" "mobilenetv2_x1_4" "shufflenetv2_x2_0"
+for model in  "mobilenetv2_x0_75" "shufflenetv2_x1_5"
 do
 	echo ${ds}_${model}_${mode}
 	python3 FeintLady.py -ds $ds -m $model --ptq_mode $mode --l1 1e-3 --l2 1e-5 --nl1 3 --nl2 50 &> ${ds}_${model}_${mode}.log
