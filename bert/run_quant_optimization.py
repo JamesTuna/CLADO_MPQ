f8c108f271e77e7cef339e4e8f552150023ee812
sayehs <sayehs@d-matrix.ai>
2023-02-13 09:15:50 -0800

    changed the optimization script

--- bert/run_quant_optimization.py
+++ bert/run_quant_optimization.py
@@ -18,8 +18,6 @@ def load_gradients(file_name):
         index = hm['layer_index'][name]
         layer_name = name[:-11]
         scheme = name[-11:]
-        a = hm['Ltilde']
-        #print(f'index {index} layer {layer_name} scheme {scheme} Ltilde {a[index,index].item():.6f}')
         index2layerscheme[index] = (layer_name,scheme)
     
     cached_grad = np.zeros_like(hm['Ltilde'])
@@ -49,7 +47,7 @@ def MIQCP_optimize(cached_grad,layer_bitops,layer_size,
     x = cp.Variable(cached_grad.shape[0], boolean=True)
     assert cached_grad.shape[0]%schemes_per_layer == 0, 'cached_gradient shape[0] does not divde schemes per layer'
     num_layers = cached_grad.shape[0]//schemes_per_layer
-    
+
     if not naive:
         # convexation of cached_grad
         es,us = np.linalg.eig(cached_grad)
@@ -74,117 +72,142 @@ def MIQCP_optimize(cached_grad,layer_bitops,layer_size,
                    layer_bitops@x/10**9<=bitops_bound,
                    layer_size@x/8/1024/1024<=size_bound]
 
+    # x = [0,1,0]*72
+    # objective = np.diagonal(cached_grad) @ x
+    # return x, objective
+    
     prob = cp.Problem(objective,constraints)
     #solver='GUROBI'
     #['ECOS', 'ECOS_BB', 'GUROBI', 'OSQP', 'SCIP', 'SCIPY', 'SCS']
-    prob.solve(solver='GUROBI', verbose=False,TimeLimit=120)
+    prob.solve(solver='GUROBI', verbose=False, TimeLimit=120)
     
     # Print result.
-    print("\nThe objective is", prob.value)
+    print("\nThe optimal value is", prob.value)
     print("\nSolution state", prob.status)
     print("A solution x is")
     print(x.value)
-    return x
+    return x, prob.value
 
        
 
 def feintLady(calib_num=128):
-
-    with open(f'./clado_mpqco_results/bert-base/Ltilde_bert-base/Ltilde_calib{calib_num}_batch0(size8).pkl','rb') as f:
+    file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Ltilde_bert-base-uncased-squad-v1/sample_size16/Ltilde_calib128_batches_0-1_bs8.pkl'
+    with open(file_name,'rb') as f:
         hm = pickle.load(f)
     ref_layer_index = hm['layer_index']
+    _, _, index2layerscheme = load_gradients(file_name)
 
-    with open('./clado_mpqco_results/bert-base/bert-base_layer-size_layer-bitops_A16-W8-4-2.pkl', 'rb') as f:
+    with open('bert-base-uncased-squad-v1_layer-size_layer-bitops_A16-W8-4-2.pkl', 'rb') as f:
         layer_bitops_size = pickle.load(f)
     layer_bitops = layer_bitops_size['layer_bitops']
     layer_size = layer_bitops_size['layer_size']
 
+
     num_batches = [2, 4, 8, 16, 32, 64, 128, 256]
-    for repeat in range(0,5,1):
+    for repeat in range(0,1,1):
         for n_batch in num_batches:
-            if repeat >= 3 and n_batch == 256:
+            if (repeat >= 3 and n_batch == 256):
                 continue
-            else:    
-                batch_Ltildes_clado = []
-                # sample size: n_batch * 8
-                for batch_id in range(n_batch):
-                    b = batch_id + n_batch * repeat
-                    with open(f'./clado_mpqco_results/bert-base/Ltilde_bert-base/Ltilde_calib{calib_num}_batch{b}(size8).pkl','rb') as f:
-                        hm = pickle.load(f)
-                    assert hm['layer_index'] == ref_layer_index
-                    batch_Ltildes_clado.append(hm['Ltilde'])
-
-                assert len(batch_Ltildes_clado) == n_batch
-                batch_Ltildes_clado_np = np.array(batch_Ltildes_clado)
-                ref_Ltilde_clado = batch_Ltildes_clado_np.mean(axis=0)
-
+            else:
                 s_batch = n_batch * repeat
                 e_batch = n_batch * repeat + n_batch - 1
                 n_smaples = n_batch * 8
-                file_name = f'./clado_mpqco_results/bert-base/Ltilde_bert-base/multiple_batches/sample_size{n_smaples}/Ltilde_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
-                if not os.path.exists(file_name):
-                    with open(file_name, 'wb') as f:
-                        pickle.dump({'Ltilde': ref_Ltilde_clado,'layer_index': ref_layer_index}, f)
+                # batch_Ltildes_clado = []
+                # #sample size: n_batch * 8
+                # for batch_id in range(n_batch):
+                #     b = batch_id + n_batch * repeat
+                #     with open(f'/homes/sayehs/clado_mpqco_results/bert-base-uncased-squad-v1/Ltilde_bert-base-uncased-squad-v1/Ltilde_calib{calib_num}_batch{b}(size8).pkl','rb') as f:
+                #         hm = pickle.load(f)
+                #     assert hm['layer_index'] == ref_layer_index
+                #     batch_Ltildes_clado.append(hm['Ltilde'])
+
+                # assert len(batch_Ltildes_clado) == n_batch
+                # batch_Ltildes_clado_np = np.array(batch_Ltildes_clado)
+                # ref_Ltilde_clado = batch_Ltildes_clado_np.mean(axis=0)
+                # file_name = f'/homes/sayehs/clado_mpqco_results/bert-base-uncased-squad-v1/Ltilde_bert-base-uncased-squad-v1/multiple_batches/sample_size{n_smaples}/Ltilde_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
+                # if not os.path.exists(file_name):
+                #     with open(file_name, 'wb') as f:
+                #         pickle.dump({'Ltilde': ref_Ltilde_clado,'layer_index': ref_layer_index}, f)
                 
-                hm, cached_grad, index2layerscheme = load_gradients(file_name)
+                # hm, cached_grad, index2layerscheme = load_gradients(file_name)
 
-                batch_deltaLs_mpqco = []
-                for batch_id in range(n_batch):
-                    b = batch_id + n_batch * repeat
-                    with open(f'./clado_mpqco_results/bert-base/DeltaL_MPQCO_bert-base/MPQCO_DELTAL_batch{b}(size8).pkl','rb') as f:
-                        hm_mpqco = pickle.load(f)
+                # batch_deltaLs_mpqco = []
+                # for batch_id in range(n_batch):
+                #     b = batch_id + n_batch * repeat
+                #     with open(f'/homes/sayehs/clado_mpqco_results/bert-base-uncased-squad-v1/DeltaL_MPQCO_bert-base-uncased-squad-v1/MPQCO_DELTAL_batch{b}(size8).pkl','rb') as f:
+                #         hm_mpqco = pickle.load(f)
 
-                    deltal = np.zeros(ref_Ltilde_clado.shape)
+                #     deltal = np.zeros(hm['Ltilde'].shape)
 
-                    for layer_id in range(len(index2layerscheme)):
-                        layer_name,scheme = index2layerscheme[layer_id]
-                        wbit = eval(scheme[:-4])[1]
-                        deltal[layer_id,layer_id] = hm_mpqco[layer_name][wbit]
-                    batch_deltaLs_mpqco.append(deltal)
+                #     for layer_id in range(len(index2layerscheme)):
+                #         layer_name,scheme = index2layerscheme[layer_id]
+                #         wbit = eval(scheme[:-4])[1]
+                #         deltal[layer_id,layer_id] = hm_mpqco[layer_name][wbit]
+                #     batch_deltaLs_mpqco.append(deltal)
                 
-                batch_deltaLs_mpqco_np = np.array(batch_deltaLs_mpqco)
-                ref_deltaLs_mpqco = batch_deltaLs_mpqco_np.mean(axis=0)
+                # batch_deltaLs_mpqco_np = np.array(batch_deltaLs_mpqco)
+                # ref_deltaLs_mpqco = batch_deltaLs_mpqco_np.mean(axis=0)
 
-                file_name = f'./clado_mpqco_results/bert-base/DeltaL_MPQCO_bert-base/multiple_batches/sample_size{n_smaples}/DeltaL_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
-                if not os.path.exists(file_name):
-                    with open(file_name, 'wb') as f:
-                        pickle.dump({'DeltaL': ref_deltaLs_mpqco,'layer_index': ref_layer_index}, f)
+                # file_name = f'/homes/sayehs/clado_mpqco_results/bert-base-uncased-squad-v1/DeltaL_MPQCO_bert-base-uncased-squad-v1/multiple_batches/sample_size{n_smaples}/DeltaL_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
+                # if not os.path.exists(file_name):
+                #     with open(file_name, 'wb') as f:
+                #         pickle.dump({'DeltaL': ref_deltaLs_mpqco,'layer_index': ref_layer_index}, f)
 
                 L = hm['Ltilde'].shape[0]
 
-                clado_res, naive_res, mpqco_res = [], [], []
+                file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Ltilde_bert-base-uncased-squad-v1/sample_size{n_smaples}/Ltilde_calib128_batches_{s_batch}-{e_batch}_bs8.pkl'
+                _, cached_grad, _ = load_gradients(file_name)
+                
+                clado_res, naive_res, mpqco_res, hawq_res = [], [], [], []
+                clado_objective, naive_objective, mpqco_objective, hawq_objective = [], [], [], []
                 size_bounds = [20.2505, 22.50055556, 24.75061111, 27.00066667, 29.25072222, 31.50077778, 33.75083333, 36.00088889, 38.25094444, 40.501, 43.39735714, 46.29371429, 49.19007143, 52.08642857, 54.98278571, 57.87914286, 60.7755, 63.67185714, 66.56821429, 69.46457143, 72.36092857, 75.25728571, 78.15364286, 81.05]
-                for size_bound in size_bounds: 
+                for size_bound in size_bounds:
                     print(f'Set size bound to {size_bound} MB')
                     #clado
-                    v1 = MIQCP_optimize(cached_grad=cached_grad,
-                                layer_bitops=layer_bitops,
-                                layer_size=layer_size,
-                                schemes_per_layer=3,
-                                bitops_bound=np.inf,size_bound=size_bound,
-                                naive=False)
+                    v1, objective_val = MIQCP_optimize(cached_grad=cached_grad,
+                                                        layer_bitops=layer_bitops,
+                                                        layer_size=layer_size,
+                                                        schemes_per_layer=3,
+                                                        bitops_bound=np.inf,size_bound=size_bound,
+                                                        naive=False)
                     clado_res.append(v1)
+                    clado_objective.append(objective_val)
                     
                     #naive
-                    v2 = MIQCP_optimize(cached_grad=cached_grad,
-                                layer_bitops=layer_bitops,
-                                layer_size=layer_size,
-                                schemes_per_layer=3,
-                                bitops_bound=np.inf,size_bound=size_bound,
-                                naive=True)
-                    naive_res.append(v2)
+                    # v2, objective_val = MIQCP_optimize(cached_grad=cached_grad,
+                    #                                 layer_bitops=layer_bitops,
+                    #                                 layer_size=layer_size,
+                    #                                 schemes_per_layer=3,
+                    #                                 bitops_bound=np.inf,size_bound=size_bound,
+                    #                                 naive=True)
+                    # naive_res.append(v2)
+                    # naive_objective.append(objective_val)
 
                     #mpqco
-                    v3 = MIQCP_optimize(cached_grad=ref_deltaLs_mpqco,
-                                layer_bitops=layer_bitops,
-                                layer_size=layer_size,
-                                schemes_per_layer=3,
-                                bitops_bound=np.inf,size_bound=size_bound,
-                                naive=True)
-                    mpqco_res.append(v3)
+                    # v3, objective_val = MIQCP_optimize(cached_grad=ref_deltaLs_mpqco,
+                    #                     layer_bitops=layer_bitops,
+                    #                     layer_size=layer_size,
+                    #                     schemes_per_layer=3,
+                    #                     bitops_bound=np.inf,size_bound=size_bound,
+                    #                     naive=True)
+                    # mpqco_res.append(v3)
+                    # mpqco_objective.append(objective_val)
 
-                with open(f'./clado_mpqco_results/bert-base/Clado_Naive_MPQCO_optimization_bert-base/sample_size{n_smaples}/clado_naive_mpqco_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
-                    pickle.dump({'clado_res': clado_res, 'naive_res': naive_res, 'mpqco_res': mpqco_res}, f)
+                    # v4, objective_val = MIQCP_optimize(cached_grad=ref_DeltaL_hawq,
+                    #                                 layer_bitops=layer_bitops,
+                    #                                 layer_size=layer_size,
+                    #                                 schemes_per_layer=3,
+                    #                                 bitops_bound=np.inf,size_bound=size_bound,
+                    #                                 naive=True)
+                    # hawq_res.append(v4)
+                    # hawq_objective.append(objective_val)
+
+
+
+                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
+                    pickle.dump({'clado_res': clado_res}, f)
+                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective.pkl','wb') as f:
+                    pickle.dump({'clado_objectives': clado_objective}, f)    
             
 def main():
     feintLady()
