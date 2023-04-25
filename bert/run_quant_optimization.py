9c3c388a708e86dd263b977ce661bd759029753d
sayehs <sayehs@d-matrix.ai>
2023-04-25 08:25:10 -0700

    Added 6k cached gradients

--- bert/run_quant_optimization.py
+++ bert/run_quant_optimization.py
@@ -103,8 +103,8 @@ def feintLady(calib_num=128):
     layer_size = layer_bitops_size['layer_size']
 
 
-    num_batches = [2, 4, 8, 16, 32, 64, 128, 256]
-    for repeat in range(0,5,1):
+    num_batches = [768, ]
+    for repeat in range(0,1,1):
         for n_batch in num_batches:
             if (repeat >= 3 and n_batch == 256):
                 continue
@@ -113,15 +113,15 @@ def feintLady(calib_num=128):
                 e_batch = n_batch * repeat + n_batch - 1
                 n_smaples = n_batch * 8
 
-                file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/DeltaL_MPQCO_bert-base-uncased-squad-v1/sample_size{n_smaples}/DeltaL_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
-                with open(file_name,'rb') as f:
-                    hm_mpqco = pickle.load(f)
-                ref_deltaLs_mpqco = hm_mpqco['DeltaL']
+                # file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/DeltaL_MPQCO_bert-base-uncased-squad-v1/sample_size{n_smaples}/DeltaL_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8.pkl'
+                # with open(file_name,'rb') as f:
+                #     hm_mpqco = pickle.load(f)
+                # ref_deltaLs_mpqco = hm_mpqco['DeltaL']
 
-                file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/DeltaL_HAWQ_bert-base-uncased-squad-v1/sample_size{n_smaples}/DeltaL_HAWQ_batches_{s_batch}-{e_batch}_bs8.pkl'
-                with open(file_name, 'rb') as f:
-                    hm_hawq = pickle.load(f)
-                ref_deltaLs_hawq = hm_hawq['DeltaL']
+                # file_name = f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/DeltaL_HAWQ_bert-base-uncased-squad-v1/sample_size{n_smaples}/DeltaL_HAWQ_batches_{s_batch}-{e_batch}_bs8.pkl'
+                # with open(file_name, 'rb') as f:
+                #     hm_hawq = pickle.load(f)
+                # ref_deltaLs_hawq = hm_hawq['DeltaL']
 
                 L = hm['Ltilde'].shape[0]
 
@@ -130,7 +130,8 @@ def feintLady(calib_num=128):
                 
                 clado_res, naive_res, mpqco_res, hawq_res = [], [], [], []
                 clado_objective, naive_objective, mpqco_objective, hawq_objective = [], [], [], []
-                size_bounds = np.linspace(35, 45, 15)
+                #size_bounds = np.linspace(35, 45, 15)
+                size_bounds = [20.2505, 22.50055556, 24.75061111, 27.00066667, 29.25072222, 31.50077778, 33.75083333, 36.00088889, 38.25094444, 40.501, 43.39735714, 46.29371429, 49.19007143, 52.08642857, 54.98278571, 57.87914286, 60.7755, 63.67185714, 66.56821429, 69.46457143, 72.36092857, 75.25728571, 78.15364286, 81.05]
                 for size_bound in size_bounds:
                     print(f'Set size bound to {size_bound} MB')
                     # clado
@@ -175,21 +176,21 @@ def feintLady(calib_num=128):
 
 
 
-                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_35-45.pkl','wb') as f:
+                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
                     pickle.dump({'clado_res': clado_res}, f)
-                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective_35-45.pkl','wb') as f:
+                with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/CLADO/Clado_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/clado_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective.pkl','wb') as f:
                     pickle.dump({'clado_objectives': clado_objective}, f)    
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/MPQCO_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/mpqco_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/MPQCO_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/mpqco_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
                 #     pickle.dump({'mpqco_res': mpqco_res}, f)
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/MPQCO_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/mpqco_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/MPQCO/MPQCO_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/mpqco_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective.pkl','wb') as f:
                 #     pickle.dump({'mpqco_objectives': mpqco_objective}, f) 
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/Naive/Naive_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/naive_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/Naive/Naive_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/naive_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
                 #     pickle.dump({'naive_res': naive_res}, f)
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/Naive/Naive_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/naive_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/Naive/Naive_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/naive_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective.pkl','wb') as f:
                 #     pickle.dump({'naive_objectives': naive_objective}, f) 
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/Hawq_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/hawq_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/Hawq_optimal_decisions_bert-base-uncased-squad-v1/sample_size{n_smaples}/hawq_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization.pkl','wb') as f:
                 #     pickle.dump({'hawq_res': hawq_res}, f)
-                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/Hawq_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/hawq_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective_35-45.pkl','wb') as f:
+                # with open(f'./clado_mpqco_results/bert-base-uncased-squad-v1/HAWQ/Hawq_optimal_objectives_bert-base-uncased-squad-v1/sample_size{n_smaples}/hawq_a16_w8-4-2_calib{calib_num}_batches_{s_batch}-{e_batch}_bs8_optimization_objective.pkl','wb') as f:
                 #     pickle.dump({'hawq_objectives': hawq_objective}, f) 
 def main():
     feintLady()
